b93f0f8ac70c0a5b0ee6fdedeb56ad193192d56e
marian-ald <marianaldescu56@yahoo.com>
2016-05-17 21:11:26 +0300

    creare ServiceAPI.cpp

---
+++ ServiceAPI/cpps/ServiceApi.cpp
@@ -0,0 +1,69 @@
+#include "../headers/ServiceAPI.h"
+
+	void Service::createUser(int id, double homeX, double homeY) {
+
+	}
+
+    void Service::createStore(int id, double storeX, double storeY) {
+
+    }
+
+    void Service::visit(int timestamp, int clientId, int storeId, int discount) {
+
+    }
+
+    void Service::invite(int userWhichInvites, int invitedUser) {
+
+    }
+
+    int Service::visitsInTimeframe(int startTime, int endTime) {
+
+    }
+
+    int Service::totalDiscountInTimeframe(int startTime, int endTime) {
+
+    }
+
+    Array<int> Service::usersWithBestBuyToDiscountRate() {
+
+    }
+
+    int Service::visitsInTimeframeOfStore(int startTime, int endTime, int storeId) {
+
+    }
+
+    Array<int> Service::biggestKDiscounts(int K, int storeId) {
+
+    }
+
+    Array<double> Service::biggestKClientDistances(int K, int storeId) {
+
+    }
+
+    Array<pair<int, int>> Service::mostCrowdedKTimeFrames(int K, int storeId) {
+
+    }
+
+    Array<int> Service::distinctGroupsOfUsers() {
+
+    }
+
+    int Service::userWithMostInvites() {
+
+    }
+
+    int Service::longestInvitesChainSize() {
+
+    }
+
+    Array<int> Service::topKGroupsWithMostVisitsOverall(int K) {
+
+    }
+
+    Array<pair<int, double>> Service::averageVisitsPerUser() {
+
+    }
+
+    pair<double, double> Service::newStoreCoordinates() {
+
+    }
